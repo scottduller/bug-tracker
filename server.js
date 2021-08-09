226bb58c63a7bb45e2b6a7fe7b5e67239fdd9800
scottduller <scottduller@gmail.com>
2021-08-09 14:07:01 +0100

    Deploy bug fix

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 const port = 5000 || process.env.PORT;
 
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 app.get('/', (req, res) => {
 	res.send('Hello World!');
+});
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
 app.listen(port, () => {
