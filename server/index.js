5d74aee9c78ebae8b66fe9f3aadd8ecb50d16f1d
Scott Duller <scottduller@gmail.com>
2021-08-10 12:04:17 +0100

    Refactored folders

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+app.get('*', (req, res) => {
+	res.sendFile(
+		path.resolve(__dirname, '../client/build', 'index.html')
+	);
+});
+
+app.listen(PORT, () => {
+	console.log(`App listening at http://localhost:${PORT}`);
+});
