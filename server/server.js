a0382cd274a0c5d52ce4a9c0c4412bc0dac2d18c
Scott Duller <scottduller@gmail.com>
2021-08-10 13:06:41 +0100

    Ammended _dirname

--- server/server.js
+++ server/server.js
@@ -6,12 +6,14 @@ const app = express();
 
 app.use(express.json());
 
+const _dirname = path.resolve();
+
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, '/client/build')));
+	app.use(express.static(path.join(_dirname, '/client/build')));
 
 	app.get('*', (req, res) =>
 		res.sendFile(
-			path.resolve(__dirname, 'client', 'build', 'index.html')
+			path.resolve(_dirname, 'client', 'build', 'index.html')
 		)
 	);
 } else {
