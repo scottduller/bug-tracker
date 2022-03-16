9d3c8ba9eacb104fe7dfab7965c47644b24d2545
Scott Duller <scottduller@gmail.com>
2022-03-16 00:48:25 +0000

    Added user auth

--- server/src/api/users/users.controllers.js
+++ server/src/api/users/users.controllers.js
@@ -1,27 +1,14 @@
 const asyncHandler = require('express-async-handler');
+const Joi = require('joi');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 const User = require('./users.model');
 
 const errMsgs = {
   notFound: 'User not found.',
+  invalidCredentials: 'User credential invalid.',
 };
-
-/**
- * @desc    Login user and get token
- * @route   POST api/users/login
- * @access  Public
- */
-const loginUser = asyncHandler(async (req, res) => {
-  res.json({ msg: 'Login User' });
-});
-
-/**
- * @desc    Register a new user
- * @route   POST api/users
- * @access  Public
- */
-const registerUser = asyncHandler(async (req, res) => {
-  res.json({ msg: 'Register User' });
-});
 
 /**
  * @desc    Get all users
@@ -41,6 +28,102 @@ const getUsers = asyncHandler(async (req, res) => {
     'last_login'
   );
   res.json(users);
+});
+
+/**
+ * @desc    Login user and get token
+ * @route   POST api/users/login
+ * @access  Public
+ */
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.query()
+    .select('id', 'first_name', 'last_name', 'email', 'password')
+    .where('email', email)
+    .first();
+  console.log(user);
+
+  if (user && bcrypt.compareSync(password, user.password)) {
+    const token = jwt.sign(
+      {
+        firstName: user.first_name,
+        lastName: user.last_name,
+        email: user.email,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: '2h',
+      }
+    );
+
+    res.json({
+      id: user.id,
+      firstName: user.first_name,
+      lastName: user.last_name,
+      email: user.email,
+      token,
+    });
+  } else {
+    res.status(401);
+    throw new Error(errMsgs.invalidCredentials);
+  }
+});
+
+/**
+ * @desc    Register a new user
+ * @route   POST api/users
+ * @access  Public
+ */
+const registerUser = asyncHandler(async (req, res) => {
+  const schema = Joi.object({
+    firstName: Joi.string().min(3).required(),
+    lastName: Joi.string().min(3).required(),
+    email: Joi.string().email(),
+    password: Joi.string()
+      .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*d)[a-zA-Zd]{8,}$/)
+      .required(),
+  });
+
+  const { error } = schema.validate(req.body);
+
+  if (error) {
+    res.status(401);
+    throw new Error(error);
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+  try {
+    const data = await User.query().insert({
+      firstName: req.body.first_name,
+      lastName: req.body.last_name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    const token = jwt.sign(
+      {
+        firstName: data.first_name,
+        lastName: data.last_name,
+        email: data.email,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: '2h',
+      }
+    );
+
+    res.status(200).json({
+      firstName: data.first_name,
+      lastName: data.last_name,
+      email: data.email,
+      token,
+    });
+  } catch (err) {
+    res.status(422);
+    throw new Error(err.constrant);
+  }
 });
 
 /**
